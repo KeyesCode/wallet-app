dd76540063d5adfc8a0e3b8836ebfad07a72e214
KeyesCode <keyescode@gmail.com>
2026-01-13 02:54:26 -0500

    Disable package exports in Metro configuration to avoid warnings with @noble/hashes and @scure/bip39, reverting to file-based resolution for compatibility.

--- metro.config.js
+++ metro.config.js
@@ -4,10 +4,13 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add support for package exports (subpath exports)
+// Disable package exports to avoid warnings with @noble/hashes and @scure/bip39
+// These packages work fine with file-based resolution (Metro's default behavior)
+// The warnings occur because Metro's package exports resolver is strict about
+// matching import paths to the exports field, even though the files exist and work
 config.resolver = {
   ...config.resolver,
-  unstable_enablePackageExports: true,
+  unstable_enablePackageExports: false,
 };
 
 module.exports = config;
